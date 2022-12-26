c816c1f24ea8465842968331d3ed616a25d73a4d
sonwebtl <46032093+sonwebtl@users.noreply.github.com>
2022-12-26 11:26:27 +0700

    Delete inc directory

--- inc/unistall.php
+++
@@ -1,9 +0,0 @@
-<?php
-function sw_deactivate_plugin() {
-  global $wpdb;
-  $table_name = $wpdb->prefix . 'chatzalosw';
-  $wpdb->query("DROP TABLE IF EXISTS $table_name");
-  print_r("unistall");
-}
-
-
